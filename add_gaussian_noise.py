9d9e7c1fd3bfa32d82909591a64e898ac5958a5b
piagorts <125029119+piagorts@users.noreply.github.com>
2024-02-26 11:57:52 +0100

    Add files via upload
    Introduce Gaussian noise to MR prostate images with different variances that can be given as input.

---
+++ add_gaussian_noise.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Introducing Gaussian noise to data
+
+@author: Pia Görts
+"""
+
+#import the needed package
+import os
+import matplotlib.pyplot as plt 
+import SimpleITK as sitk
+
+#%%
+#Function to perform gaussian blurring with a given variance
+
+def gaussian_blur(args):
+    """
+    Parameters
+    ----------
+    args : input arguments given by user, first argument is the fullpath
+    of the input file, second argument is the variance (sigma) of the desired gaussian blur 
+
+    Returns
+    -------
+    blur_array : the Gaussian blurred image
+    
+    Packages needed
+    -------
+    import sys
+    import SimpleITK as sitk
+
+    """
+
+    reader = sitk.ImageFileReader()
+    reader.SetFileName(args[0])
+    input_image = reader.Execute()
+
+    pixelID = input_image.GetPixelID()
+
+    gaussian = sitk.SmoothingRecursiveGaussianImageFilter()
+    gaussian.SetSigma(float(args[1]))
+    blur_image = gaussian.Execute(input_image)
+
+    caster = sitk.CastImageFilter()
+    caster.SetOutputPixelType(pixelID)
+    blur_image = caster.Execute(blur_image)
+    
+    # Convert SimpleITK images to NumPy arrays
+    blur_array = sitk.GetArrayFromImage(blur_image)
+    
+    return blur_array
+
+
+#%%
+#use the function gaussian blur on the prostate images, plot first the original image
+# and then 3 times a blurred image with increasing variance
+
+FOLDER_PATH = r'D:\Documenten\Master\Q3\Capita selecta image analysis\Elastix'
+DATA_PATH = r'D:\Documenten\Master\Q3\Capita selecta image analysis\Data\TrainingData'
+ELASTIX_PATH = os.path.join(FOLDER_PATH, 'elastix.exe')
+TRANSFORMIX_PATH = os.path.join(FOLDER_PATH, 'transformix.exe')
+
+
+subfolders = os.listdir(DATA_PATH)[1:]
+filenames = ["mr_bffe", "mr_bffe.zraw","prostaat","prostaat.zraw"]
+
+for i in range(len(subfolders)):
+    patient = subfolders[i]
+    sigma = [1,1.5,2]
+    currentpath_mr = os.path.join(DATA_PATH, patient, 'mr_bffe.mhd')
+    image_mr = sitk.ReadImage(currentpath_mr)
+    array_mr = sitk.GetArrayFromImage(image_mr)
+    
+    depth = 50
+    fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+    ax[0].imshow(array_mr[depth,:,:], cmap='gray')
+    ax[0].set_title('mr image not blurred')
+    
+    #Blur the image for 3 different variances of Gaussian blur
+    blur_arrays = []
+    for j in range(len(sigma)):
+        args = [currentpath_mr,sigma[j]]
+        blur_array = gaussian_blur(args)
+        blur_arrays.append(blur_array)
+        ax[j+1].imshow(blur_array[depth,:,:], cmap='gray')
+        ax[j+1].set_title(f'mr image blurred, variance {sigma[j]}')
+
+
+
+
+    
+
+
+
